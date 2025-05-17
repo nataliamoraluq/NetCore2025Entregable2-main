13dbfe9c83fcecebfc6825144283f843a5318c63
nataliamoraluq <mora.luq.natalia68@gmail.com>
2025-05-16 23:48:50 -0400

    prueba de endpoints, todos funcionando

--- Infrastructure/Repositories/UserRepository.cs
+++ Infrastructure/Repositories/UserRepository.cs
@@ -5,11 +5,29 @@ using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces.Repositories;
 using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories
 {
-    public class UbicacionRepository: BaseRepository<Ubicacion>, IUbicacionRepository
+    public class UserRepository: BaseRepository<User>, IUserRepository
     {
-        public UbicacionRepository(AppDbContext context) : base(context) {}
+        public UserRepository(AppDbContext context) : base(context) {}
+        // ***!!!! -> modif para el met especifico de buscar por user y password
+
+        //login?
+        public async ValueTask<User> GetUser(string UserName){
+            //*!
+            //string UserName, string Password
+            //get user: 
+            return await base.dbSet.FirstOrDefaultAsync(usuario => usuario.UserName == UserName);
+            //
+        }
+
+        //search by id -> el async ValueTask a usar es el del Base Repository
+
+        /*public override async ValueTask<User> GetByIdAsync(string UserName, string Password)
+        {
+            return await base.dbSet.Include(x => x.UserName).FirstAsync(x => x.UserName == UserName && x.Password == Password);
+        }*/
     }
 }
